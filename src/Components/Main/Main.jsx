db8331c13b8d26db2a0a1dddb1c9d1c72f1b0aa8
Zharkynuly Yedil <erden.aidynuly.99@gmail.com>
2021-05-20 02:35:36 +0600

    last commit

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -19,19 +19,60 @@ import Button from '@material-ui/core/Button';
 import { AuthContext, UserDataContext } from "../../App";
 import {useCookies} from 'react-cookie';
 import { useEffect, useContext } from "react";
+import ReactPlayer from 'react-player'
+import AddIcon from '@material-ui/icons/Add';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 
 function Main(props){
   const {cookieJWT} =  useContext(AuthContext);
   const {userData, setuserData} = useContext(UserDataContext)
+  const [genrelist, setgenrelist] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [serials, setSerials] = useState([]);
+  const [new_movie, setNewMovies] = useState([]);
+  const [last_movie, setLastMovie] = useState([]);
 
+ 
     useEffect(() => {
       console.log(cookieJWT)
       if (cookieJWT['jwt']!==undefined){
         console.log(cookieJWT)
-        test(props)
+        test(props);
+        loadMovie();
+        console.log(last_movie?.actors);    
+
       }
   }, []);
   
+  async function loadMovie(){
+    const bearer = "Bearer "+cookieJWT['jwt'].jwtToken;
+    const response = await fetch("http://localhost:8000/api/", {
+        method:'GET',
+        headers: {
+        "Content-Type": "application/json",
+        "Authorization": bearer
+        }
+    });
+        if(response.status===200){
+            let res = await response.json();
+                if (res.popular_movies.length > 0){
+                  setPopularMovies(res.popular_movies);
+                }
+                if( res.serials.length >0){
+                  setSerials(res.serials);
+                }
+                if(res.new_movie.length>0){
+                  setNewMovies(res.new_movie);
+                }
+                if(res.genres.length>0){
+                  setgenrelist(res.genres);
+                }
+                if(res.last_movie !== undefined){
+                  setLastMovie(res.last_movie);
+                }
+        }
+  }
+
   async function test(props){   
     console.log(cookieJWT['jwt'])
     const bearer = "Bearer "+cookieJWT['jwt'].jwtToken;
@@ -42,12 +83,45 @@ function Main(props){
         "Authorization": bearer
         }
     });
-        if(response.status==200){
+        if(response.status===200){
             let res = await response.json();
             console.log(res);
             setuserData(res);
         }    
     }
+    const settingsSerials = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+      };
     const settings = {
       dots: true,
       infinite: true,
@@ -115,6 +189,8 @@ function Main(props){
           }
         ]
         };
+
+        
     return (
         <>
         <div className="row mt-5">
@@ -122,44 +198,91 @@ function Main(props){
                 <Card style={{ width: '10rem' }}>
                     <Card.Header>Genre</Card.Header>
                     <ListGroup variant="flush">
-                        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                    { genrelist.length>0 && genrelist.map((row) => (
+                      <Link to={`/admin/genre/${row.id}`} style={{textDecoration:"none", color:"black"}}>
+                          <ListGroup.Item>{row.name}</ListGroup.Item>
+                        </Link>
+                    ))}
                     </ListGroup>
                     </Card>
             </div>
             <div className="col-md-10">
-                        <Player
-                            playsInline
-                            poster="/assets/poster.png"
-                            height="10"
-                            width="90"
-                            src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-                            />
-                    
+              <div className="col">
+                  <div className='row player-wrapper1' style={{height:"400px"}}>
+                    <div className="col-6 title-wrapper ">
+                        <div className="playerTitle">
+                            <p>{last_movie.name}</p>
+                            <p className="underplayerTitle">В главных ролях:</p>
+                            <p className="actorName">{last_movie.actors?.map((row) => (row.full_name+" "))}</p>
+                            <p className="underplayerTitle">Режиссер:</p><p className="actorName">{last_movie.producer}</p>
+                            <strong className="watchWith">Смотрите по подписке</strong>
+                            <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className="col-6 offset-3 mt-4"
+                            style={{width:"150px", marginRight:"10px"}}
+                            >
+                            <PlayArrowIcon/>
+                            Смотреть
+                          </Button>
+                          <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="#ccff90"
+                            className="col-6 offset-3 mt-4"
+                            style={{width:"350px"}}
+                            >
+                            <AddIcon/>
+                            Буду смотреть
+                          </Button>
+                        </div>  
+                    </div>
+                      <ReactPlayer
+                      config={{
+                        youtube: {
+                          playerVars: { showinfo: 1 }
+                        },
+                        facebook: {
+                          appId: '12345'
+                        }
+                      }}
+                        className='react-player video-wrapper col-6'
+                        url='https://youtu.be/rVn2NCgCVLU'
+                        width='100%'
+                        height='100%'
+                      />
+                    </div>
+                  </div>
+                        
 
-                <h2 className="mt-5 ml-2 bold" style={{fontWeight:"bold"}}>Popular movies</h2>
+                <h2 className="mt-5 ml-2 bold" style={{fontWeight:"bold"}}>Выбор редакции</h2>
                 <Slider {...settings}>
-                <div className="p-2">
-                    <img height="200px" width="100%" src="https://uzfilm.net/wp-content/uploads/2021/02/71369237.jpg" />
-                </div>
-                <div className="p-2">
-                    <img height="200px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                </div>
-                <div className="p-2">
-                    <img height="200px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                </div>
-                <div className="p-2">
-                  <img height="200px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                </div>
-                <div className="p-2">
-                    <img height="200px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                </div>
-                <div className="p-2">
-                    <img height="200px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                </div>
+                {
+                popularMovies.length <= 0 && <h3>Пока никто  не добавил объявление <img src="https://freepikpsd.com/wp-content/uploads/2019/10/apple-emoji-png-pack-6-Free-PNG-Images-Transparent.png" width="50px" ></img></h3>
+                }
+                  { popularMovies.length>0 && popularMovies.map((row) => (
+                    <div className="p-2 moviecard">
+                      {row.rating <= 7 && row.rating >4 &&
+                      <p className="text-block1" style={{backgroundColor:"orange"}}>{row.rating}</p>
+                      }
+                    { row.rating > 7 &&
+                      <p className="text-block1" style={{backgroundColor:"green"}}>{row.rating}</p>
+                    }
+                    {
+                      row.rating <= 4 &&
+                      <p className="text-block1" style={{backgroundColor:"red"}}>{row.rating}</p>
+                    }
+                        <img height="200px" width="100%" src={row.small_picture} />
+                        <a className="mt-2 moviecardtitle">{row.name}</a>
+                        <p className="moviecardundertitle">{row?.genres[0]?.name}</p>
+                    </div>
+                  ))
+                }
                 </Slider>
-
+                
                 <div className="MainBar mt-5 ml-2">
                   <div className="MainBar2">
                     <h3 className="mainbarTitle text-center">Тысячи фильмов и сериалов на КиноПоиск HD</h3>
@@ -174,68 +297,37 @@ function Main(props){
                     Subscribe
                   </Button>
                     <Slider className="mt-5" {...settings1}>
-                  <div>
-                      <img height="180px" width="100%" src="https://uzfilm.net/wp-content/uploads/2021/02/71369237.jpg" />
-                      <p className="p">Title</p>
-                  </div>
-                  <div className="p-2">
-                    <img height="180px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                    <p className="p">Title</p>
-                  </div>
-                  <div className="p-2">
-                    <img height="180px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                    <p className="p">Title</p>
-                  </div>
-                  <div className="p-2">
-                    <img height="180px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                    <p className="p">Title</p>
-                  </div>
-                  <div className="p-2">
-                    <img height="180px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                    <p className="p">Title</p>
-                  </div>
-                  <div className="p-2">
-                    <img height="180px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                    <p className="p">Title</p>
-                  </div>
-                  <div className="p-2">
-                    <img height="180px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                    <p className="p">Title</p>
-                  </div>
-                  <div className="p-2">
-                    <img height="180px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                    <p className="p">Title</p>
-                  </div>
-                  <div className="p-2">
-                    <img height="180px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                    <p className="p">Title</p>
-                  </div>
-                  <div className="p-2">
-                    <img height="180px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                    <p className="p">Title</p>
-                  </div>
+                    { new_movie?.map((row) => (
+                      <div className="p-2 movieNewcard">
+                      {row.rating <= 7 && row.rating >4 &&
+                      <p className="text-block1" style={{backgroundColor:"orange"}}>{row.rating}</p>
+                      }
+                    { row.rating > 7 &&
+                      <p className="text-block1" style={{backgroundColor:"green"}}>{row.rating}</p>
+                    }
+                    {
+                      row.rating <= 4 &&
+                      <p className="text-block1" style={{backgroundColor:"red"}}>{row.rating}</p>
+                    }
+                        <img height="180px" width="100%" src={row.small_picture} />
+                        <a href={`/movie/${row.id}`} className="mt-2 moviecardtitledark">{row.name}</a>
+                        <p className="moviecardundertitle">{row?.genres[0]?.name}</p>
+                    </div>
+                    
+                    ))}
                   </Slider>
 
                   <h3 style={{color:"white", marginTop:"20px", fontWeight:"bold"}}>Выбираем фильмы и сериалы</h3>
                   <Slider {...settings}>
-                <div className="p-2">
-                    <img height="200px" width="100%" src="https://uzfilm.net/wp-content/uploads/2021/02/71369237.jpg" />
-                </div>
-                <div className="p-2">
-                    <img height="200px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                </div>
-                <div className="p-2">
-                    <img height="200px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                </div>
-                <div className="p-2">
-                  <img height="200px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                </div>
-                <div className="p-2">
-                    <img height="200px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                </div>
-                <div className="p-2">
-                    <img height="200px" width="100%" src="https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/57e25896-b6e8-47af-966e-feab17041298/300x450" />
-                </div>
+                  {
+                serials.length <= 0 && <h3>Пока никто  не добавил объявление <img src="https://freepikpsd.com/wp-content/uploads/2019/10/apple-emoji-png-pack-6-Free-PNG-Images-Transparent.png" width="50px" ></img></h3>
+                }
+                  { serials.length>0 && serials.map((row) => (
+                    <div className="p-2 movieNewcard">
+                        <img height="200px" width="100%" src={row.small_picture} />
+                    </div>
+                  ))
+                }
                 </Slider>
                 </div>
                 </div>
@@ -243,8 +335,10 @@ function Main(props){
             </div>
       
         </>
+    
 
     );
+   
 }
 
 export default Main;
--- src/Components/Main/MovieDetails.jsx
+++ src/Components/Main/MovieDetails.jsx
@@ -15,7 +15,12 @@ import Container from '@material-ui/core/Container';
 import { useState } from "react";
 import {useCookies} from 'react-cookie';
 import { useParams} from "react-router-dom";
-
+import { useEffect, useContext } from "react";
+import { AuthContext, UserDataContext } from "../../App";
+import ReactPlayer from 'react-player'
+import AddIcon from '@material-ui/icons/Add';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import { Card } from "react-bootstrap";
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
@@ -52,12 +57,38 @@ const useStyles = makeStyles((theme) => ({
 export default function MovieDetails() {
           let {MovieId} = useParams();
           const classes = useStyles();
-
+          const {cookieJWT} =  useContext(AuthContext);
+          const {userData, setuserData} = useContext(UserDataContext)
           const [email, setEmail] = useState("");
           const [password, setPassword] = useState("");
           const [repassword, setRepassword] = useState("");
           const [full_name, setFull_name] = useState("");
-          const [cookieJWT, setCookieJWT, removeCookieJWT] = useCookies(['jwt']);
+          const [cookieJWT1, setCookieJWT, removeCookieJWT] = useCookies(['jwt']);
+
+          useEffect(() => {
+            if (cookieJWT['jwt']!==undefined){
+              console.log(cookieJWT)
+              test();
+            }
+        }, []);
+
+
+          async function test(){   
+            console.log(cookieJWT['jwt'])
+            const bearer = "Bearer "+cookieJWT['jwt'].jwtToken;
+            const response = await fetch("http://localhost:8000/api/profile", {
+                method:'GET',
+                headers: {
+                "Content-Type": "application/json",
+                "Authorization": bearer
+                }
+            });
+                if(response.status===200){
+                    let res = await response.json();
+                    console.log(res);
+                    setuserData(res);
+                }    
+            }
 
           const handleEmailChange = event =>{
                     setEmail(event.target.value);
@@ -78,12 +109,11 @@ export default function MovieDetails() {
 
           const inputData = {email, password, full_name};
                     if(repassword===password){
-                              register(inputData);
-                    
-                              }else{
-                                        alert('enter correct repassword')
-                              }
-                              event.preventDefault();
+                          register(inputData);
+                          }else{
+                                    alert('enter correct repassword')
+                          }
+              event.preventDefault();
           }
 
           async function register(data){
@@ -106,93 +136,32 @@ export default function MovieDetails() {
                     }
           }
   
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign Up
-        </Typography>
-        <form className={classes.form} noValidate  onSubmit={handleSubmit}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            onChange={handleEmailChange}
-            autoFocus
-          />
-
-        <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="text"
-            label="Full Name"
-            name="full_name"
-            autoComplete="text"
-            onChange={handleFullnameChange}
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            onChange={handlePasswordChange}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="repassword"
-            label="Re-Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            onChange={handleRepasswordChange}
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
-  );
+      return (
+      <Container component="main">
+        <div className="row">
+          <Card.Header className="col-4 card">
+            <div className="col mt-5 mb-4">
+              <img height="420px" width="100%" src="https://kinopoisk-ru.clstorage.net/1Yp4O3234/01fb72SD/DXV37pRW8_bCn9kiu5SBHcKz5cLLdDNVLdrWUOlPpFhugUIEeWEIsWdNn4t65ZaeSTqtJyrcATzItTbnNARxN3wQo84Qx4cy59usp0qANP23UoQCi4RDQSkvIxQ46yEsD1B3oayT-7PkbwipGRcUnI8uySMrCdTpTLQTd_A00ONtZ4UvMlMAdCO9uAE5oLKvIUVkEEu0E0S5iiSBfv3EamshorQI8RiQdS6pOQkWxi9eT4iQDMv0ucx101UQZIbDT6f2W0OxYMbwbYmwmHNTrVF2BCF6hqMn-_j2I-6txDoLEpB326JokwQN-Fpql9XOfZwbQNzKF9sM9UPmYEQ2Qt6ENIsRkWJnIp64UrpCg3_AhUbBm4bDxYlLdxLeP1Y7GABRtxvRmADFTQlJeSeVrr_dmLJ_mGSavOSTBkCnxYDcpMV7o4LDFWCee2LbktL88WZ28ZsUMccqmyRz34z3SYrDQ9fY0Xpj5u9L2soG5rx9XsqzTulFCb6FwZQANSQgfNTmi2PjgTdDvjvxapCSTCAVpxOo9SNkCxtnQf8ehiuKg7HEuhLrY9buSFrqdtauXU7qcv2pNJvNVjKlIzaVUhwEVstBMYNG0c4JctgAga2wB6VA-rUDhotrRDPc3ETYOhDR1GuAaoPFbkp42JXkL78tyWKPCac4jReCFVN3VjKNlVV6McOQlTLeKkDZIyJeAddEAYiEs2W7GEUSDoxGqgoxMhYJQKlDdLyKWGi09H48nsly3_uEmf5lcyeyZ1SCrRVGmbHQMVTgnkkCygMgntMnRVOJZ9Gma-slkq8_Vlg4gMOEC1GoAIVfqSso9IeNPv05IO2rFpsuNnKmY_RFII5393qQ4GB2Ah4ZIgvDcM_j54aRmSUiZzmLJWLfbRTbW9NQdhrzWjGVD2lrOaSmXg5s-MIcmsbZD8fxF5HmlBBsR1X4cmChxtB--iDKwqEswiXGQmj30ibJuxTCvZ-0qijy8HWKAQpRJrwqCZll9Mw9vTiwHxmma58FQUYTRgYQvRUUSyMCcISCfetQmeEjXWFV5UIJNIHWyhjmEt7eVIu4MTCG21DaoHQveSlIZvfMr54qY3yoRzvNR-NHUCUXQiy2lGpAc2MnMLx6MUhwId1CxoYyCBQiVvkoFJEt_ubbmUOixPuzGRFn3avIqld0Dz7OGkAuyFQInTQwlmF3dwN_Ftf4QSISxLEt22HKYhG8E_SW4ZinYTYpaXWijj5WGYhzsIZ68pvhJ_9paEsE52-N71pwjMt2G4x1cpVSB_SyvraGeQDgUYUDj6oxacNRbAO19oI7BoA2mBj1czyPBIuacrHmCSD4sebPy4va9VZeLW6qAB8ZBsitV_LWARfkMG1FB8viIIF1gd66Y3qTYU1y5BVC6Wfjh_hrNlO9zxZpiWFwJQoyexMWz_oJ-JVEn57euOLuqyd7jydxx6Pk9nF-ZEf5grFgxYO9uDLaYnOu4AaU4as1EXQqu7SQzC_lC0vDkcZKkEjA9s_YG5iExs1vfeoBD0uUag1nsiWSRcWT33aUSVJjUObgXgtw66Ain_PmV1HZ1LBGmqpGU3yfBRsIQ0GU6BBbURSfiRpLR2ZefU1YwCxppQlshFAk8TYE0s7HFKmC8GBksR5YM4gSM00BdHUj60fgh9hq1LKujUbLCHBSJtuDi9Dl7Hs5GVeXnK-dKSA9KPdIvLWB9TAUBCD8VJSZ49DARpBeC2FIgVO8owSEAKrEkoYLmJYwPW13aCnTIYQLQCrC5O_Li5gXpm49D_jzDIgnKS8l4ufCRuTRbqd2unPgcGQyfrnxOTAir1LUNrJ7N0H2exr1Mq1thCk6sZIWWWMpUWaNCcn5NNX9T93qwG9KV1oOV2EUUfYFMP03NVuigRMnUN5JcLrTwy2ztybSSMajR8oq9nDMzvVpSULy9iiASwNG3AkZiqfn7C3sqEIsSjU6rsXAJaEXR2CuJVX7wJJg9sItiCF7kPDc80XFY0v2glS4eFRg7a9EuHvw0-Rp4vkTlU9pq0h0J5xd_osAbPnGKg_EchbzdpQy3tTUu0DTQxUgbMmBGOPhDbE1pvAKxeAmyCq04N3tFonb8eOFydM70uXdiNuYVBWvT6978u-rJesMNYFng3T1QP2XZlqQwlFFwg_psBnQs70CldVCu1RTxZgLJrDOv6ULORJwtPkBacBlL_nIeKdn7O3cOSI8uHZYrXSwx8AVVzPfdzaZkdAiliHtOeE4QELfQhY08eqnY0bb20ZBPK21a1sx4EXawKpQx1xYypoGpP097XgzTcsU-U8mEgeBxxTCP1Umq7Jwg5Vg3LsB6uADPXCkl1KbFjGk6cjnII0NNul40TOXqaBrUsfdO3tKhTefv3yY8c-qJemf5UEF8WbGQg8Hd_hR8FLFMG37wYrA8m4hNkVCyrYgNIl4hTI93PcKO9Nx5KtRWjNkHQgq66b3vE7Ni1HPmSa77tfSt5I3VJEetlRrEJDypOL82rKag1MMIWSF8eoFY9WZ2LZj7q02WWgS4GcYgOpwlvxY-2qX1p1s_PqQDsvFaCy0IyQDNKYSbQUl-QCCY9dyD6ogO8MibqDH5HPJ51OGmHhlMN9c1vgLQFHmKfEK0MSOifjbFUe-jVz6k_27dXk-xpD1oKXHUz62JImys2JFAR3IItiAQ0yDppcASLSAdYgJdmD_jfQaewNT9rrTmXHUngkZyyaWngyPm3Md-CRZTyQwFnAGNXHMlIeL4GKD1zOfulE4ExH80OXUQkjFQiSZynYxfw12qmkTEwRpQtnQ5L5L-7sXB52NL5iibplE2N7Vg7VARTawvucUGZCCQTUQnipwC5Pg_AEX1sHp9SJXiap1oP3cxzmLUqOmWQDIoScMCPsJdSVsfE_IwT9pFNjfBJFm88SUUlxlNftTgzKG8Jx6McrQwU7BV_ST61ZBtQtLFMDffLT76BCi1gjAqHLEvnvbi1bl7M0-iBFd2UdLnMVSs"/>
+              <div className='player-wrapper' style={{height:"180px"}}>
+              <ReactPlayer
+                      config={{
+                        youtube: {
+                          playerVars: { showinfo: 1 }
+                        },
+                        facebook: {
+                          appId: '12345'
+                        }
+                      }}
+                        className='react-player video-wrapper mt-4'
+                        url='https://youtu.be/rVn2NCgCVLU'
+                        width='100%'
+                        height='100%'
+                      />
+              </div>
+          </div>
+          </Card.Header>
+          <Card.Header className="col-8">This is</Card.Header>
+        </div>
+      </Container>
+      );
 }
